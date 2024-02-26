e65218f546c5d10789d2fa3d8b4fabad97c8edfb
Github11200 <78944617+Github11200@users.noreply.github.com>
2024-02-25 21:36:18 -0800

    More notes

---
+++ Typescript/02-Basics/myInterface.js
@@ -0,0 +1,12 @@
+var aUser = {
+    dbId: 22,
+    email: "h@h.com",
+    userId: 2211,
+    startTrial: function () {
+        return "trial started";
+    },
+    getCoupon: function (name, off) {
+        return 10;
+    },
+};
+aUser.email = "h@hc.com";
--- Typescript/02-Basics/myInterface.ts
+++ Typescript/02-Basics/myInterface.ts
@@ -1,6 +1,44 @@
 interface UserInterface {
+    readonly dbId: number;
     email: string;
     userId: number;
+    googleId?: string;
+    // startTrial: () => string;
+    startTrial(): string; // This is another way of also defining a method
+    getCoupon(couponName: string, value: number): number;
 }
 
-const aUser: UserInterface = { email: "h@h.com", userId: 2211 };
+// Some people call it the "reopening" of the interface,
+// you can basically just add more values
+interface UserInterface {
+    githubToken: string;
+}
+
+// You can also extend interfaces, you can also add another
+// interface by adding a comma after UserInterface and then
+// putting the name of the seconnd one
+interface Admin extends UserInterface {
+    role: "admin" | "ta" | "learner";
+}
+
+const aUser: Admin = {
+    dbId: 22,
+    role: "admin",
+    email: "h@h.com",
+    userId: 2211,
+    githubToken: "github",
+    startTrial: () => {
+        return "trial started";
+    },
+    getCoupon: (name: "goofy", off: 10) => {
+        return 10;
+    },
+};
+
+aUser.email = "h@hc.com";
+
+// DIFFERENCES BETWEEN TYPE ALIASES AND INTERFACES
+/*
+    -> It is easier to add more properties to interfaces syntax wise
+    -> Type aliases can't be extended
+*/
